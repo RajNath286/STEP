b615a7fb3ff83857808d6b1400735935f3afe48a
Raj Nath <rajnath6790@gmail.com>
2026-09-23 19:40:06 +0530

    Added Week 4 codes

---
+++ week4/A1ProductExceptSelf.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class A1ProductExceptSelf {
+
+    static int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        int leftProduct = 1;
+
+        // Left product
+        for (int i = 0; i < n; i++) {
+            answer[i] = leftProduct;
+            leftProduct = leftProduct * nums[i];
+        }
+
+        int rightProduct = 1;
+
+        // Right product
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = answer[i] * rightProduct;
+            rightProduct = rightProduct * nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = productExceptSelf(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
